95f59d2e511d2d8e87a630a37aa58adc3a434a0a
Sergei Jeihala <jey@tut.by>
2016-04-26 13:50:19 +0300

    Initial commit

---
+++ modules/windows_tts/windows_tts.class.php
@@ -0,0 +1,175 @@
+<?php
+/**
+* Windows TTS 
+* @package project
+* @author Wizard <sergejey@gmail.com>
+* @copyright http://majordomo.smartliving.ru/ (c)
+* @version 0.1 (wizard, 13:03:10 [Mar 13, 2016])
+*/
+//
+//
+class windows_tts extends module {
+/**
+* windows_tts
+*
+* Module class constructor
+*
+* @access private
+*/
+function windows_tts() {
+  $this->name="windows_tts";
+  $this->title="Windows TTS";
+  $this->module_category="<#LANG_SECTION_APPLICATIONS#>";
+  $this->checkInstalled();
+}
+/**
+* saveParams
+*
+* Saving module parameters
+*
+* @access public
+*/
+function saveParams($data=0) {
+ $p=array();
+ if (IsSet($this->id)) {
+  $p["id"]=$this->id;
+ }
+ if (IsSet($this->view_mode)) {
+  $p["view_mode"]=$this->view_mode;
+ }
+ if (IsSet($this->edit_mode)) {
+  $p["edit_mode"]=$this->edit_mode;
+ }
+ if (IsSet($this->tab)) {
+  $p["tab"]=$this->tab;
+ }
+ return parent::saveParams($p);
+}
+/**
+* getParams
+*
+* Getting module parameters from query string
+*
+* @access public
+*/
+function getParams() {
+  global $id;
+  global $mode;
+  global $view_mode;
+  global $edit_mode;
+  global $tab;
+  if (isset($id)) {
+   $this->id=$id;
+  }
+  if (isset($mode)) {
+   $this->mode=$mode;
+  }
+  if (isset($view_mode)) {
+   $this->view_mode=$view_mode;
+  }
+  if (isset($edit_mode)) {
+   $this->edit_mode=$edit_mode;
+  }
+  if (isset($tab)) {
+   $this->tab=$tab;
+  }
+}
+/**
+* Run
+*
+* Description
+*
+* @access public
+*/
+function run() {
+ global $session;
+  $out=array();
+  if ($this->action=='admin') {
+   $this->admin($out);
+  } else {
+   $this->usual($out);
+  }
+  if (IsSet($this->owner->action)) {
+   $out['PARENT_ACTION']=$this->owner->action;
+  }
+  if (IsSet($this->owner->name)) {
+   $out['PARENT_NAME']=$this->owner->name;
+  }
+  $out['VIEW_MODE']=$this->view_mode;
+  $out['EDIT_MODE']=$this->edit_mode;
+  $out['MODE']=$this->mode;
+  $out['ACTION']=$this->action;
+  $this->data=$out;
+  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+  $this->result=$p->result;
+}
+/**
+* BackEnd
+*
+* Module backend
+*
+* @access public
+*/
+function admin(&$out) {
+ $this->getConfig();
+ $out['DISABLED']=$this->config['DISABLED'];
+ if ($this->view_mode=='update_settings') {
+   global $disabled;
+   $this->config['DISABLED']=$disabled;
+   $this->saveConfig();
+   $this->redirect("?ok=1");
+ }
+
+ if ($_GET['ok']) {
+  $out['OK']=1;
+ }
+ 
+ /*
+ global $clean;
+ if ($clean) {
+    array_map("unlink", glob(ROOT . "cached/voice/*_yandex.mp3"));
+    $this->redirect("?ok=1");
+ } 
+ */
+}
+
+/**
+* FrontEnd
+*
+* Module frontend
+*
+* @access public
+*/
+function usual(&$out) {
+ $this->admin($out);
+}
+
+ function processSubscription($event, $details='') {
+  $this->getConfig();
+  if ($event=='SAY' && !$this->config['DISABLED'] && (!$details['ignoreVoice'])) {
+    $level=$details['level'];
+    $message=$details['message'];
+    if ($level >= (int)getGlobal('minMsgLevel') && IsWindowsOS())
+    {
+       safe_exec('cscript ' . DOC_ROOT . '/rc/sapi.js ' . $message, 1, $level);
+    }
+  }
+ }
+/**
+* Install
+*
+* Module installation routine
+*
+* @access private
+*/
+ function install($data='') {
+  subscribeToEvent($this->name, 'SAY');
+  parent::install();
+ }
+// --------------------------------------------------------------------
+}
+/*
+*
+* TW9kdWxlIGNyZWF0ZWQgTWFyIDEzLCAyMDE2IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
+*
+*/
